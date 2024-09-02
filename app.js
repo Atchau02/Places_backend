54614e8cba5173dd9b54ebff7737ee8cfa54302b
Anh Tu Chau <anhtuchau99@gmail.com>
2024-09-02 15:53:32 -0400

    Init files

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use("/api/places", placesRoutes);
+
+app.use("/api/users", usersRoutes);
+
+app.use((req, res, next) => {
+  throw new HttpError("Could not find this route.", 404);
+});
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occured." });
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://anhtu:airW0VKqEoGwk6Ud@cluster0.dzirv.mongodb.net/mern?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    app.listen(8080);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
